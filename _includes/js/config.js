4a4e400328b785de9bbf0a8aad4d8d535419dc2d
Morten Høfft <mhoefft@gbif.org>
2022-03-09 14:23:09 +0100

    feat: update configuration schema. See https://github.com/gbif/hosted-portals/issues/221

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -1,21 +1,32 @@
-var siteTheme = gbifReactComponents.themeBuilder.extend({baseTheme: 'light', extendWith: {
-  primary: themeStyle.colors.primary
-}});
+var siteTheme = gbifReactComponents.themeBuilder.extend({
+  baseTheme: 'light', extendWith: {
+    primary: themeStyle.colors.primary
+  }
+});
 
 var siteConfig = {
-  rootPredicate: {
-    "type": "and",
-    "predicates": [
-      {
-        "type": "in",
-        "key": "countryCode",
-        "values": ["NZ"]
-      },
-      {
-        "type": "equals",
-        "key": "notIssues",
-        "value": "COUNTRY_COORDINATE_MISMATCH"
-      }
-    ]
+  routes: {
+    occurrenceSearch: {
+      // The route you are currently using for occurrence search. The language prefix will be added automatically
+      // If you need special routes per language, then you have to add locale specific overwrites. The page language is available as a global variable called `pageLang`
+      route: '/data'
+    }
+  },
+  occurrence: {
+    rootPredicate: {
+      "type": "and",
+      "predicates": [
+        {
+          "type": "in",
+          "key": "countryCode",
+          "values": ["NZ"]
+        },
+        {
+          "type": "equals",
+          "key": "notIssues",
+          "value": "COUNTRY_COORDINATE_MISMATCH"
+        }
+      ]
+    }
   }
 };
