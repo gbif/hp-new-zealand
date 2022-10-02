b804f52ce9cc189351d82a71f20a4b65a60f6c81
Aaron Wilton <wiltona@landcareresearch.co.nz>
2022-10-03 10:40:56 +1300

    #20  update to data.table configuration

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -5,11 +5,12 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({
 });
 
 var siteConfig = {
+  
   routes: {
     occurrenceSearch: {
       // The route you are currently using for occurrence search. The language prefix will be added automatically
       // If you need special routes per language, then you have to add locale specific overwrites. The page language is available as a global variable called `pageLang`
-      route: '/data'
+        route: '/data'
     }
   },
   occurrence: {
@@ -18,6 +19,8 @@ var siteConfig = {
       lng: 172.2973463,
       zoom: 3.5
     },
+    excludedFilters: ['country'],
+    defaultTableColumns: ['features', 'coordinates', 'year', 'basisOfRecord', 'dataset', 'publisher', 'catalogNumber'], // the first column will always be scientificName, that cannot be changed
     rootPredicate: {
       "type": "and",
       "predicates": [
