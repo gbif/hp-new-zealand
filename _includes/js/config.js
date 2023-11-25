61973b014b5713d97e7b7029f2fc6ca216c95589
Aaron Wilton <wiltona@landcareresearch.co.nz>
2023-11-26 11:56:28 +1300

    add more fields to availableTableColumns

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -21,7 +21,7 @@ var siteConfig = {
     },
     excludedFilters: ['country'],
     highlightedFilters: ['typeStatus'],
-    availableTableColumns: ['features', 'coordinates', 'year', 'basisOfRecord', 'dataset', 'publisher', 'catalogNumber'], // the first column will always be scientificName, that cannot be changed
+    availableTableColumns: ['features', 'coordinates', 'year', 'basisOfRecord', 'dataset', 'publisher', 'catalogNumber', 'typeStatus', 'institutionCode', 'collectionCode', 'recordedBy'], // the first column will always be scientificName, that cannot be changed
     defaultTableColumns: ['features', 'coordinates', 'year', 'basisOfRecord', 'dataset', 'publisher', 'catalogNumber'], // the first column will always be scientificName, that cannot be changed
     rootPredicate: {
       "type": "and",
