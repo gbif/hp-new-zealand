44080e29f093d9c4b3001f304f5081af1859bf06
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2026-09-16 20:55:38 +0200

    update config format

--- _includes/js/config.js
+++ _includes/js/config.js
@@ -5,31 +5,64 @@ var siteTheme = gbifReactComponents.themeBuilder.extend({
 });
 
 var siteConfig = {
-  
-  routes: {
-    occurrenceSearch: {
-      // The route you are currently using for occurrence search. The language prefix will be added automatically
-      // If you need special routes per language, then you have to add locale specific overwrites. The page language is available as a global variable called `pageLang`
-        route: '/data'
+  "version": 3,
+  "pages": [
+    {
+      "id": "occurrenceSearch",
+      "path": "data"
+    }
+  ],
+  "disableInlineTableFilterButtons": false,
+  "availableCatalogues": [
+    "OCCURRENCE"
+  ],
+  "dataHeader": {
+    "enableApiPopup": false,
+    "enableInfoPopup": false
+  },
+  "theme": {
+    "primary": "#cc6600",
+    "borderRadius": 3,
+    "stickyOffset": "0px"
+  },
+  "maps": {
+    "mapStyles": {
+      "defaultProjection": "MERCATOR",
+      "defaultMapStyle": "BRIGHT",
+      "options": {
+        "MERCATOR": [
+          "BRIGHT",
+          "NATURAL"
+        ]
+      }
     }
   },
-  occurrence: {
-    mapSettings: {
-      lat: -40.4291568,
-      lng: 172.2973463,
-      zoom: 3.5
-    },
-    excludedFilters: ['country'],
-    highlightedFilters: ['basisOfRecord', 'taxonKey', 'year', 'occurrenceStatus', 'occurrenceIssue', 'typeStatus'],
-    //availableTableColumns: ['features', 'coordinates', 'year', 'basisOfRecord', 'dataset', 'publisher', 'catalogNumber', 'typeStatus', 'institutionCode', 'collectionCode', 'recordedBy'], // the first column will always be scientificName, that cannot be changed
-    defaultTableColumns: ['features', 'coordinates', 'year', 'basisOfRecord', 'dataset', 'publisher', 'catalogNumber'], // the first column will always be scientificName, that cannot be changed
-    rootPredicate: {
+  "languages": [
+    {
+      "code": "en",
+      "localeCode": "en",
+      "label": "English",
+      "default": true,
+      "textDirection": "ltr",
+      "reactIntlLocale": "en-GB",
+      "iso3LetterCode": "eng",
+      "cmsLocale": "en-GB",
+      "vocabularyLocale": "en",
+      "gbifOrgLocalePrefix": "",
+      "mapTileLocale": "en"
+    }
+  ],
+  "messages": {},
+  "occurrenceSearch": {
+    "scope": {
       "type": "and",
       "predicates": [
         {
           "type": "in",
           "key": "country",
-          "values": ["NZ"]
+          "values": [
+            "NZ"
+          ]
         },
         {
           "type": "equals",
@@ -37,6 +70,36 @@ var siteConfig = {
           "value": "COUNTRY_COORDINATE_MISMATCH"
         }
       ]
+    },
+    "highlightedFilters": [
+      "basisOfRecord",
+      "taxonKey",
+      "year",
+      "occurrenceStatus",
+      "issue",
+      "typeStatus"
+    ],
+    "excludedFilters": [
+      "country"
+    ],
+    "defaultEnabledTableColumns": [
+      "features",
+      "coordinates",
+      "year",
+      "basisOfRecord",
+      "dataset",
+      "publisher",
+      "catalogNumber"
+    ],
+    "mapSettings": {
+      "lat": -40.4291568,
+      "lng": 172.2973463,
+      "zoom": 3.5
     }
-  }
+  },
+  "collectionSearch": {},
+  "institutionSearch": {},
+  "datasetSearch": {},
+  "publisherSearch": {},
+  "literatureSearch": {}
 };
